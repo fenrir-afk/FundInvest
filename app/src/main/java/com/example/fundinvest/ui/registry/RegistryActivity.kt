8a8213b7a0dcf4e4beb9a19dc0a9836dfd580c67
Stepan <Fenrird@inbox.ru>
2024-05-22 12:07:13 +0300

    create login and registration layouts

---
+++ app/src/main/java/com/example/fundinvest/ui/registry/RegistryActivity.kt
@@ -0,0 +1,17 @@
+package com.example.fundinvest.ui.registry
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.fundinvest.R
+
+class RegistryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_registry)
+
+    }
+}
